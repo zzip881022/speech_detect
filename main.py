5df6f5e5c90cb3e43b42b881047baf49983434db
WU898989 <wujx89@gmail.com>
2022-11-19 23:17:06 +0800

    login - 模型辨識完成後把音檔刪除

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from email.mime import audio
 from flask import Flask, jsonify, request, render_template
 from torch_utils import get_prediction, set_using_model, audio_to_numpy_mfcc
 import shutil
+import os
+import time
 import mimetypes
 
 mimetypes.add_type('text/css', '.css')
@@ -9,13 +11,16 @@ mimetypes.add_type('application/javascript', '.js')
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/login')
 def home():
     return render_template('login.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
+    time.sleep(2)
     file_source = 'C:/Users/Alice/Downloads/'
     file_destination = 'C:/Users/Alice/Desktop/Graduate-project/flask/pytorch-flask-tutorial/app'
     if request.method == 'POST':
@@ -27,16 +32,27 @@ def predict():
         prediction = get_prediction()
         # data = {'prediction': prediction.item(), 'verify':verify_model, 'audio':audio_file}
         
+    time.sleep(3)
+
+    # 辨識完後刪除檔案
+    try:
+        os.remove(file_destination + '/' + audio_file)
+    except OSError as e:
+        print(e)
+    else:
+        print("File is deleted successfully")
+        
         return prediction
 
-        # return render_template('predict.html', data=('虛假語音','真實語音')[prediction.item()==1])
-        
-        # # try:
-        # #     prediction = get_prediction()
-        # #     data = {'prediction': prediction.item()}
-        # #     return jsonify(data)
-        # # except:
-        # return jsonify({'error': 'error during prediction'})
+    # return render_template('predict.html', data=('虛假語音','真實語音')[prediction.item()==1])
+
+    # # try:
+    # #     prediction = get_prediction()
+    # #     data = {'prediction': prediction.item()}
+    # #     return jsonify(data)
+    # # except:
+    # return jsonify({'error': 'error during prediction'})
+
 
 @app.route('/chat')
 def chat():
